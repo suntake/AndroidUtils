d8de3706a368ef846875534801ce41b8ff3bdcf4
suntake <a.nimkarde@gmail.com>
2014-01-10 13:04:30 +0530

    Update Utils_snipplets.java

--- Utils_snipplets.java
+++ Utils_snipplets.java
@@ -1,8 +1,18 @@
 
-
   Utils 
   
-  	requestWindowFeature(Window.FEATURE_NO_TITLE);
-		getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
-				WindowManager.LayoutParams.FLAG_FULLSCREEN);
+  make an activity full screen? I mean without the notification bar.
+  
+      requestWindowFeature(Window.FEATURE_NO_TITLE);
+      getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN,
+						WindowManager.LayoutParams.FLAG_FULLSCREEN);
+				
+	OR using XML
+	
+	<activity android:name=".ActivityName"
+	          android:label="@string/app_name"
+	          android:theme="@android:style/Theme.NoTitleBar.Fullscreen">
+	</activity>
+				
+				
   
