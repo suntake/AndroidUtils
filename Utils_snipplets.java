3b86823c48bc862079f36ce868e6e2679762e0f2
suntake <a.nimkarde@gmail.com>
2014-01-10 13:07:00 +0530

    Update Utils_snipplets.java

--- Utils_snipplets.java
+++ Utils_snipplets.java
@@ -17,8 +17,29 @@
 				
 //*************************************************************************************				
   
+  Android Splash Screen Using Timer
+  
+  
+  in oncreate use below snipplet
 
-
+  new Handler().postDelayed(new Runnable() {
+ 
+            /*
+             * Showing splash screen with a timer. This will be useful when you
+             * want to show case your app logo / company
+             */
+ 
+            @Override
+            public void run() {
+                // This method will be executed once the timer is over
+                // Start your app main activity
+                Intent i = new Intent(SplashScreen.this, MainActivity.class);
+                startActivity(i);
+ 
+                // close this activity
+                finish();
+            }
+        }, SPLASH_TIME_OUT);
 
 
 
